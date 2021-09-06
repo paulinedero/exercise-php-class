029870152fbc7214f5d5b3bf247a7ecd45e807a1
Pauline De Ro <pauline@bobex.net>
2021-09-06 10:44:19 +0200

    realized exercice with php-class and heritage

--- Bicycle.php
+++ Bicycle.php
@@ -1,69 +1,7 @@
 <?php
-class Bicycle
+
+require_once 'Vehicle.php';
+class Bicycle extends Vehicle
 {
-    //properties
-    private /*string*/ $color;
-    private /*int*/ $currentSpeed;
-    private /*int*/ $nbSeats = 1;
-    private /*int*/ $nbWheels = 2;
-
-    //constructor
-    public function __construct(string $color, int $currentSpeed, int $nbSeats, int $nbWheels)
-    {
-        $this->color = $color;
-        $this->currentSpeed = $currentSpeed;
-        $this->nbSeats = $nbSeats;
-        $this->nbWheels = $nbWheels;
-    }
-
-    //methods
-    public function forward(): string
-    {
-        $this->currentSpeed = 15;
-        return "Go !";
-    }
-
-    public function brake(): string
-    {
-        $sentence = "";
-        while ($this->currentSpeed > 0) {
-            $this->currentSpeed--;
-            $sentence .= "Brake !!!";
-        }
-        $sentence .= "I'm stopped !";
-        return $sentence;
-    }
-
-    //getters
-    public function getColor(): string
-    {
-        return $this->color;
-    }
-
-    public function getCurrentSpeed(): int
-    {
-        return $this->currentSpeed;
-    }
-
-    public function getNbSeats(): int
-    {
-        return $this->nbSeats;
-    }
-
-    public function getNbWheels(): int
-    {
-        return $this->nbWheels;
-    }
-
-    //setters
-    public function setColor(string $color): void
-    {
-        $this->color = $color;
-    }
-
-    public function setCurrentSpeed(int $currentSpeed): void
-    {
-        $this->currentSpeed = $currentSpeed;
-    }
 }
 ?>
